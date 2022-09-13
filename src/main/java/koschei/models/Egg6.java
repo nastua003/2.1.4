1b854396f48d2aa5572fd1c56e47857786a70fac
Anastasiya <n.domasevich06@gmail.com>
2022-09-13 16:53:34 +0300

    Hello

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,7 +1,16 @@
 package koschei.models;
 
-public class Egg6 {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
+public class Egg6 {
+    private Needle7 needle;
+
+    @Autowired
+    public void Egg6(Needle7 needle) {
+        this.needle = needle;
+    }
     @Override
     public String toString() {
         return ", в яйце иголка " + "";
